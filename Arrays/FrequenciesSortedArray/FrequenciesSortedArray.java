8a9d4bfb48958bcd7874f5a65d1ab00b566e5bad
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2023-05-01 06:12:35 +0530

    Arrays init

--- Arrays/FrequenciesSortedArray/FrequenciesSortedArray.java
+++ Arrays/FrequenciesSortedArray/FrequenciesSortedArray.java
@@ -1,4 +1,23 @@
 public class FrequenciesSortedArray {
 
-  public static void main(String[] args) {}
+  static void countFreq(int arr[]) {
+    int freq = 1, i = 1;
+    while (i < arr.length) {
+      while (i < arr.length && arr[i] == arr[i - 1]) {
+        freq++;
+        i++;
+      }
+      System.out.print(arr[i - 1] + " " + freq);
+      i++;
+      freq = 1;
+    }
+    if (arr.length == 1 || arr[arr.length - 1] != arr[arr.length - 2]) {
+      System.out.println(arr[arr.length - 1] + " " + 1);
+    }
+  }
+
+  public static void main(String[] args) {
+    int arr[] = {};
+    countFreq(arr);
+  }
 }
