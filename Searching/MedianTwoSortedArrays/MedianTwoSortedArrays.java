f71dfa77e3938236375eead4dc0dbe1516d93497
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2023-05-07 07:53:54 +0530

    medianTwoSortedArray

---
+++ Searching/MedianTwoSortedArrays/MedianTwoSortedArrays.java
@@ -0,0 +1,13 @@
+class MedianTwoSortedArrays {
+
+  static int medianArraysNaive(int a1[], int a2[]) {
+    int n1 = a1.length;
+    int n2 = a2.length;
+    while (n1 <= n2) {
+      int mid = (n1 + n2) / 2;
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {}
+}
