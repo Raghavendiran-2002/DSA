aa25c6af23d39a62f0549dc5b3008814db46036d
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2023-05-28 18:55:19 +0530

    CheckifStringsRotations

---
+++ String/CheckifStringsRotations/CheckifStringsRotations.java
@@ -0,0 +1,16 @@
+public class CheckifStringsRotations {
+
+  public static boolean arerotations(String s1, String s2) {
+    if (s1.length() != s2.length()) {
+      return false;
+    }
+    String temp = s1 + s1;
+    return temp.indexOf(s2) != -1;
+  }
+
+  public static void main(String[] args) {
+    String s1 = "ABCD";
+    String s2 = "CDAB";
+    System.out.println(arerotations(s1, s2));
+  }
+}
