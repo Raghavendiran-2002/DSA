c898a1dc9b905974fffb0990d2a0afe1fcce0bc4
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2023-06-02 07:41:59 +0530

    LongestSubstringWithDistinctElementNaive

--- String/LongestSubstringWithDistinctElement/LongestSubstringNaive.java
+++ String/LongestSubstringWithDistinctElement/LongestSubstringNaive.java
@@ -1,5 +1,29 @@
-class LongestSubstring{
- psvm
+class LongestSubstringNaive {
 
+  static boolean areDistinct(String str, int i, int j) {
+    boolean visited[] = new boolean[256];
 
-import java utii}
+    for (int k = i; k <= j; k++) {
+      if (visited[str.charAt(k)] == true) return false;
+      visited[str.charAt(k)] = true;
+    }
+    return true;
+  }
+
+  static int longestDistinct(String str) {
+    int n = str.length();
+    int res = 0;
+    for (int i = 0; i < n; i++) for (int j = i; j < n; j++) if (
+      areDistinct(str, i, j)
+    ) res = Math.max(res, j - i + 1);
+    return res;
+  }
+
+  public static void main(String args[]) {
+    String str = "geeksforgeeks";
+    int len = longestDistinct(str);
+    System.out.print(
+      "The length of the longest distinct characters substring is " + len
+    );
+  }
+}
