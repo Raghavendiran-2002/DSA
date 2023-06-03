c442ecba8ed7d810fbdd7569b27de9ac474c0f55
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2023-06-03 14:44:35 +0530

    PatternSearchingNaive

--- String/PatternSearching/PatternSearchingNaive.java
+++ String/PatternSearching/PatternSearchingNaive.java
@@ -6,8 +6,20 @@ public class PatternSearchingNaive {
     for (int i = 0; i <= (n - m); i++) {
       int j;
       for (j = 0; j < m; j++) if (pat.charAt(j) != txt.charAt(i + j)) break;
-
       if (j == m) System.out.print(i + " ");
+    }
+  }
+
+  static void patSearchinng2(String txt, String pat) {
+    int m = pat.length();
+    int n = txt.length();
+    for (int i = 0; i <= (n - m);) {
+      int j;
+      for (j = 0; j < m; j++) {
+        if (pat.charAt(j) != txt.charAt(i + j)) break;
+        if (j == m) System.out.println(i + " ");
+        if (j == 0) i++; else i = (i + j);
+      }
     }
   }
 
@@ -16,5 +28,7 @@ public class PatternSearchingNaive {
     String pat = "ABCD";
     System.out.print("All index numbers where pattern found: ");
     patSearchinng(txt, pat);
+    System.out.println();
+    patSearchinng2(txt, pat);
   }
 }
